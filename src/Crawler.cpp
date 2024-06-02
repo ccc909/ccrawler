9ccb5d86d180ac7b9cf2c4de75738ff3e1f891fc
mihai moldovan <nowaynany@gmail.com>
2024-06-02 23:27:45 +0300

    use only stack allocations

--- src/Crawler.cpp
+++ src/Crawler.cpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include "ThreadPool.cpp"
 #include <nlohmann/json.hpp>
@@ -7,232 +7,142 @@
 #include <chrono>
 #include <thread>
 #include <unordered_set>
-#include <fstream>
+#include <iostream>
+#include <memory>
 #include "Link.cpp"
 #include "Domain.cpp"
 #include <ixwebsocket/IXWebSocket.h>
-#include <iostream>
-
 
 using json = nlohmann::json;
 
 class Crawler {
 public:
-	//init crawler with websocket and thread pool
-	Crawler(ix::WebSocket* webSocket) : m_pool(std::thread::hardware_concurrency()), m_webSocket(webSocket) {
-	}
+    Crawler(ix::WebSocket* webSocket) : m_pool(std::thread::hardware_concurrency()), m_webSocket(webSocket) {}
 
-	std::unordered_map<std::string, Domain*> m_visitedDomains;
+    // Crawl function to be called by the websocket
+    void crawl(const Link& startLink) {
+        auto domain = std::make_unique<Domain>(startLink);
 
+        std::unordered_map<std::string, std::unique_ptr<Domain>> visitedDomains;
+        std::unordered_set<std::string> visitedUrls;
+        std::unordered_set<std::string> testset;
 
-	//crawl function, should be called by the websocket
-	void crawl(const Link& startLink) {
-		Domain* domain;
+        visitedDomains[startLink.getDomain()] = std::move(domain);
 
-		//try to find a previously created domain, also should help to avoid double crawl??
-		auto it = m_visitedDomains.find(startLink.getDomain());
-		if (it != m_visitedDomains.end()) {
-			domain = it->second;
-		}
-		else {
-			domain = new Domain(startLink);
-			m_visitedDomains[startLink.getDomain()] = domain;
-		}
+        while (visitedDomains[startLink.getDomain()]->hasLinks() && !m_shouldStop) {
+            auto [link, depth] = visitedDomains[startLink.getDomain()]->getNextLink();
 
+            std::string domainName = link.getDomain();
+            std::lock_guard<std::mutex> lock(m_mutexes[domainName]);
 
-		//might miss some links, but good enough
-		while (domain->hasLinks() && !m_shouldStop) {
-			//if (m_shouldStop) {
-			//    break; // Exit the loop
-			//}
+            std::string fullLink = link.getFullLink();
 
-			//get next link
-			auto [link, depth] = domain->getNextLink();
+            if (visitedUrls.count(fullLink) != 0 || depth >= 10) {
+                continue;
+            }
 
-			std::string domainName = link.getDomain();
-			std::lock_guard<std::mutex> lock(m_mutexes[domainName]); // Lock access to the queue for this domain
+            visitedUrls.insert(fullLink);
 
-			std::string fullLink = link.getFullLink();
+            cpr::Response response = cpr::Get(cpr::Url{ fullLink });
 
-			if (m_visitedUrls.count(fullLink) != 0 || depth >= 10) {
-				//skip if URL has already been visited or depth limit reached
-				continue;
-			}
+            if (response.status_code == 200) {
+                std::cout << "Successfully crawled: " << fullLink << std::endl;
 
-			//insert visited link
-			m_visitedUrls.insert(fullLink);
+                std::set<std::string> links = extractUrls(response.text, fullLink);
 
-			cpr::Response response = cpr::Get(cpr::Url{ fullLink });
+                for (const auto& childLink : links) {
+                    Link child(childLink);
+                    std::cout << childLink << std::endl;
 
-			if (response.status_code == 200) {
-				std::cout << "Successfully crawled: " << fullLink << std::endl;
+                    if (visitedDomains[startLink.getDomain()]->canCrawl(child)) {
+                        if (child.getDomain() != domainName) {
+                            json jsonData;
+                            jsonData["message_type"] = "new_domain";
+                            jsonData["parent_domain"] = domainName;
+                            jsonData["child_domain"] = child.getDomain();
+                            m_webSocket->sendText(jsonData.dump());
 
-				// extract URLs using regex
-				std::set<std::string> links = extractUrls(response.text, fullLink);
+                            m_pool.enqueue([this, child] { crawl(child); });
+                        }
+                        else {
+                            json jsonData;
+                            jsonData["message_type"] = "new_link";
+                            jsonData["childLink"] = childLink;
+                            std::string jsonStr = jsonData.dump();
 
-				// add child URLs to the BFS queue
-				for (const auto& childLink : links) {
-					Link child(childLink);
-					std::cout << childLink << std::endl;
-					if (domain->canCrawl(child)) {
-						//should immediatelly return if same domain is visited??? maybe add another check , it doesnt stop
+                            visitedDomains[startLink.getDomain()]->addLink(child, depth + 1);
 
-						//!!!!!!!!!!!!!!!!!!!!!!!!need one more condition to check if domain already has a thread running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-						if (child.getDomain() != domainName) {
-							json jsonData;
-							jsonData["message_type"] = "new_domain";
-							jsonData["parent_domain"] = domainName;
-							jsonData["child_domain"] = child.getDomain();
-							m_webSocket->sendText(jsonData.dump());
-							// submit crawling task to the thread pool for URLs from a new domain
-							m_pool.enqueue([this, child] { crawl(child); });
-						}
-						else {
-							json jsonData;
-							jsonData["message_type"] = "new_link";
-							jsonData["childLink"] = childLink;
+                            if (testset.count(childLink) == 0) {
+                                m_webSocket->sendText(jsonStr);
+                            }
+                            testset.insert(childLink);
+                        }
+                    }
+                }
+            }
+            else {
+                std::cerr << "Failed to crawl: " << fullLink << std::endl;
+                std::cerr << "Status code: " << response.status_code << std::endl;
+            }
 
-							// Convert JSON object to string
-							std::string jsonStr = jsonData.dump();
+            std::this_thread::sleep_for(std::chrono::milliseconds(visitedDomains[startLink.getDomain()]->getCrawlDelay()));
+        }
+    }
 
-							domain->addLink(child, depth + 1);
-							//cheap fix for now but works, cralwer still might be doing double work in background but frontend receives correct data
-							if (testset.count(childLink) == 0)
-								m_webSocket->sendText(jsonStr);
-							testset.insert(childLink);
-						}
-					}
-				}
-			}
-			else {
-				std::cerr << "Failed to crawl: " << fullLink << std::endl;
-				std::cerr << "Status code: " << response.status_code << std::endl;
-			}
+    void queueFromWebsocket(const std::string& url) {
+        Link link(url);
+        m_pool.enqueue([this, link] { crawl(link); });
+    }
 
-			//delay crwaling per domain
-			std::this_thread::sleep_for(std::chrono::milliseconds(domain->getCrawlDelay()));
-		}
-	}
+    void stop() {
+        {
+            std::unique_lock<std::mutex> lock(m_pool.queueMutex);
+            m_shouldStop = true;
+            m_pool.stop = true;
+        }
+    }
 
-	void queueFromWebsocket(const std::string& url) {
-		Link link(url);
-		m_pool.enqueue([this, link] { crawl(link); });
-	}
-
-	//unused
-	void saveState(const std::string& filename) {
-		std::ofstream file(filename);
-		if (file.is_open()) {
-			file << serializeState();
-			file.close();
-			std::cout << "Crawling session state saved to file: " << filename << std::endl;
-		}
-		else {
-			std::cerr << "Failed to save crawling session state to file: " << filename << std::endl;
-		}
-	}
-
-	//unused
-	void resumeState(const std::string& filename) {
-		std::ifstream file(filename);
-		if (file.is_open()) {
-			std::string state;
-			std::getline(file, state);
-			deserializeState(state);
-			file.close();
-			std::cout << "Crawling session state resumed from file: " << filename << std::endl;
-		}
-		else {
-			std::cerr << "Failed to resume crawling session state from file: " << filename << std::endl;
-		}
-	}
-
-
-	void stop() {
-		{
-			std::unique_lock<std::mutex> lock(m_pool.queueMutex);
-			m_shouldStop = true; // Set the flag to indicate that the crawler should stop
-			m_pool.stop = true; // Set the pool's stop flag
-		}
-
-
-		m_visitedDomains.clear();
-		m_visitedUrls.clear();
-		testset.clear();
-	}
-
-	void setParams(bool _ignoreRobots, bool _domainsOnly) {
-		ignoreRobots = _ignoreRobots;
-		domainsOnly = _domainsOnly;
-	}
+    void setParams(bool _ignoreRobots, bool _domainsOnly) {
+        ignoreRobots = _ignoreRobots;
+        domainsOnly = _domainsOnly;
+    }
 
 private:
-	bool ignoreRobots = false;
-	bool domainsOnly = false;
+    bool ignoreRobots = false;
+    bool domainsOnly = false;
 
-	//always true for now, wanted to use to limit crawler to only domains, but not needed anymore
-	bool should_crawl(const std::string& link) {
-		return true;
-	}
+    bool should_crawl(const std::string& link) {
+        return true;
+    }
 
-	//extract urls from html 
-	std::set<std::string> extractUrls(const std::string& html, const std::string& baseUrl) {
-		std::set<std::string> links;
-		std::regex linkRegex("<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1", std::regex::icase);
-		auto words_begin = std::sregex_iterator(html.begin(), html.end(), linkRegex);
-		auto words_end = std::sregex_iterator();
-		for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
-			std::smatch match = *i;
-			std::string link = match.str(2);
-			if (!link.empty() && link[0] != '#') {
-				if (link.find("http") != 0) {
-					if (!baseUrl.empty() && baseUrl.back() == '/')
-						link = baseUrl.substr(0, baseUrl.length() - 1) + link;
-					else
-						link = baseUrl + link;
-				}
-				// Normalize the URL to remove any trailing slash
-				if (!link.empty() && link.back() == '/')
-					link.pop_back();
-				links.insert(link);
-			}
-		}
-		return links;
-	}
+    std::set<std::string> extractUrls(const std::string& html, const std::string& baseUrl) {
+        std::set<std::string> links;
+        std::regex linkRegex("<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1", std::regex::icase);
+        auto words_begin = std::sregex_iterator(html.begin(), html.end(), linkRegex);
+        auto words_end = std::sregex_iterator();
+        for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+            std::smatch match = *i;
+            std::string link = match.str(2);
+            if (!link.empty() && link[0] != '#') {
+                if (link.find("http") != 0) {
+                    if (!baseUrl.empty() && baseUrl.back() == '/') {
+                        link = baseUrl.substr(0, baseUrl.length() - 1) + link;
+                    }
+                    else {
+                        link = baseUrl + link;
+                    }
+                }
+                if (!link.empty() && link.back() == '/') {
+                    link.pop_back();
+                }
+                links.insert(link);
+            }
+        }
+        return links;
+    }
 
-	//unused
-	std::string serializeState() {
-		std::ostringstream oss;
-
-		// Serialize visited URLs
-		oss << m_visitedUrls.size() << "\n";
-		for (const auto& url : m_visitedUrls) {
-			oss << url << "\n";
-		}
-
-		return oss.str();
-	}
-
-	//unused
-	void deserializeState(const std::string& state) {
-		std::istringstream iss(state);
-
-		// Deserialize visited URLs
-		size_t numVisitedUrls;
-		iss >> numVisitedUrls;
-		m_visitedUrls.clear();
-		for (size_t i = 0; i < numVisitedUrls; ++i) {
-			std::string url;
-			iss >> url;
-			m_visitedUrls.insert(url);
-		}
-	}
-
-	std::mutex m_callbackMutex;
-	std::map<std::string, std::mutex> m_mutexes; // Mutexes for each domain to synchronize access to queues
-	std::unordered_set<std::string> m_visitedUrls;
-	std::unordered_set<std::string> testset;
-	ThreadPool m_pool;
-	ix::WebSocket* m_webSocket;
-	bool m_shouldStop = false;
+    std::map<std::string, std::mutex> m_mutexes;
+    ThreadPool m_pool;
+    ix::WebSocket* m_webSocket;
+    bool m_shouldStop = false;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,107 @@
-
 #include <ixwebsocket/IXWebSocketServer.h>
 #include "Crawler.cpp"
+#include <optional>
 
 using json = nlohmann::json;
 
-int main(){
-ix::initNetSystem();
-int port = 9001;
-std::string host("127.0.0.1");
-ix::WebSocketServer server(port, host, ix::SocketServer::kDefaultTcpBacklog, 1);
-Crawler *crawler;
-bool isStarted = false;
+int main() {
+    ix::initNetSystem();
+    int port = 9001;
+    std::string host("127.0.0.1");
+    ix::WebSocketServer server(port, host, ix::SocketServer::kDefaultTcpBacklog, 1);
 
-server.setOnClientMessageCallback([&](std::shared_ptr<ix::ConnectionState> connectionState, ix::WebSocket& webSocket, const ix::WebSocketMessagePtr& msg) {
-    std::cout << "Remote ip: " << connectionState->getRemoteIp() << std::endl;
-    
+    std::optional<Crawler> crawler;
+    bool isStarted = false;
 
-    if (msg->type == ix::WebSocketMessageType::Open)
-    {
-        
-        crawler = new Crawler(&webSocket);
-        
-        std::cout << "New connection" << std::endl;
+    server.setOnClientMessageCallback([&](std::shared_ptr<ix::ConnectionState> connectionState, ix::WebSocket& webSocket, const ix::WebSocketMessagePtr& msg) {
+        std::cout << "Remote ip: " << connectionState->getRemoteIp() << std::endl;
 
-        std::cout << "id: " << connectionState->getId() << std::endl;
+        if (msg->type == ix::WebSocketMessageType::Open) {
+            crawler.emplace(&webSocket);
 
-        // The uri the client did connect to.
-        std::cout << "Uri: " << msg->openInfo.uri << std::endl;
+            std::cout << "New connection" << std::endl;
+            std::cout << "id: " << connectionState->getId() << std::endl;
+            std::cout << "Uri: " << msg->openInfo.uri << std::endl;
 
-        std::cout << "Headers:" << std::endl;
-        for (auto it : msg->openInfo.headers)
-        {
-            std::cout << "\t" << it.first << ": " << it.second << std::endl;
+            std::cout << "Headers:" << std::endl;
+            for (auto it : msg->openInfo.headers) {
+                std::cout << "\t" << it.first << ": " << it.second << std::endl;
+            }
         }
-    }
-	else if (msg->type == ix::WebSocketMessageType::Message)
-	{
-        std::cout << "Received: " << msg->str << std::endl;
+        else if (msg->type == ix::WebSocketMessageType::Message) {
+            std::cout << "Received: " << msg->str << std::endl;
 
-        std::string message = msg->str;
+            std::string message = msg->str;
+            if (!message.empty() && message.front() == '"' && message.back() == '"') {
+                message = message.substr(1, message.size() - 2);
+            }
 
-		if (!message.empty() && message.front() == '"' && message.back() == '"') {
-			message = message.substr(1, message.size() - 2);
-		}
-
-        json jsonMsg;
-
-        try
-        {
-            jsonMsg = json::parse(message);
-        }
-        catch (json::parse_error& ex)
-        {
-            json parseException;
-            parseException["error"] = "parse_error";
-            std::cout<<"Error parsing json: " << ex.what() << std::endl;
-            webSocket.sendText(parseException.dump());
-            return;
-        }
-
-        if (jsonMsg["action"] == "start") {
-            isStarted = true;
-            bool domainsOnly = false;
-            bool ignoreRobots = false;
-
-            if (jsonMsg.contains("params")) {
-				for (auto& [key, value] : jsonMsg["params"].items()) {
-					if (key == "domainsOnly") {
-						domainsOnly = value;
-					}
-					else if (key == "ignoreRobots") {
-						ignoreRobots = value;
-					}
-				}
-			}
-
-            std::string domain = jsonMsg["domain"];
-
-            crawler->setParams(ignoreRobots, domainsOnly);
-
-            crawler->queueFromWebsocket(domain);
-        }
-        else if (jsonMsg["action"] == "stop") {
-            if (!isStarted) {
+            json jsonMsg;
+            try {
+                jsonMsg = json::parse(message);
+            }
+            catch (json::parse_error& ex) {
+                json parseException;
+                parseException["error"] = "parse_error";
+                std::cout << "Error parsing json: " << ex.what() << std::endl;
+                webSocket.sendText(parseException.dump());
                 return;
             }
-                
-            
-			json stopstatus;
-			stopstatus["action"] = "stop_start";
-            webSocket.sendText(stopstatus.dump());
-			crawler->stop();
-            stopstatus["action"] = "stop_end";
-            webSocket.sendText(stopstatus.dump());
-			delete crawler;
-			crawler = nullptr;
-			crawler = new Crawler(&webSocket);
-            isStarted = false;
-		}
 
-		/*if (msg->str == "stop")
-		{
-			crawler->stop();
-			delete crawler;
-			crawler = nullptr;
-			crawler = new Crawler(&webSocket);
-		}
-		else {
-			crawler->queueFromWebsocket(message);
-		}*/
-	}
-    else if (msg->type == ix::WebSocketMessageType::Close)
-    {
-		std::cout << "Closed connection" << std::endl;
-		crawler->stop();
-		delete crawler;
-        crawler = nullptr;
-	}
-    else if (msg->type == ix::WebSocketMessageType::Error)
-    {
-		std::cout << "Error: " << msg->errorInfo.reason << std::endl;
-	}
-});
+            if (jsonMsg["action"] == "start") {
+                isStarted = true;
+                bool domainsOnly = false;
+                bool ignoreRobots = false;
 
+                if (jsonMsg.contains("params")) {
+                    for (auto& [key, value] : jsonMsg["params"].items()) {
+                        if (key == "domainsOnly") {
+                            domainsOnly = value;
+                        }
+                        else if (key == "ignoreRobots") {
+                            ignoreRobots = value;
+                        }
+                    }
+                }
 
+                std::string domain = jsonMsg["domain"];
+                crawler->setParams(ignoreRobots, domainsOnly);
+                crawler->queueFromWebsocket(domain);
+            }
+            else if (jsonMsg["action"] == "stop") {
+                if (!isStarted) {
+                    return;
+                }
 
-auto res = server.listen();
-if (!res.first)
-{
-    std::cerr << "Error occurred while listening: " << res.second << std::endl;
-    return 1;
-}
+                json stopstatus;
+                stopstatus["action"] = "stop_start";
+                webSocket.sendText(stopstatus.dump());
+                crawler->stop();
+                stopstatus["action"] = "stop_end";
+                webSocket.sendText(stopstatus.dump());
+                crawler.reset();
+                crawler.emplace(&webSocket);
+                isStarted = false;
+            }
+        }
+        else if (msg->type == ix::WebSocketMessageType::Close) {
+            std::cout << "Closed connection" << std::endl;
+            if (crawler) {
+                crawler->stop();
+            }
+            crawler.reset();
+        }
+        else if (msg->type == ix::WebSocketMessageType::Error) {
+            std::cout << "Error: " << msg->errorInfo.reason << std::endl;
+        }
+    });
 
-server.start();
+    auto res = server.listen();
+    if (!res.first) {
+        std::cerr << "Error occurred while listening: " << res.second << std::endl;
+        return 1;
+    }
 
-server.wait();
-
-
+    server.start();
+    server.wait();
 }
